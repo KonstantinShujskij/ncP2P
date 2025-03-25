7c253d2770816f1573d7de337ce78a05a3903dde
Konstantin <shujskij.konstantin@gmail.com>
2025-03-25 19:04:43 +0100

    Fix | Make Payment Autoclear

--- client/src/pages/CreatePayment/CreatePayment.js
+++ client/src/pages/CreatePayment/CreatePayment.js
@@ -10,10 +10,10 @@ import useAlert from '../../hooks/alert.hook'
 
 
 function CreatePayment() {
-  const amount = useInput(0) 
+  const amount = useInput('') 
   const card = useInput('') 
   const refId = useInput('') 
-  const course = useInput(0) 
+  const course = useInput('') 
 
   const Payment = usePaymentApi()
   const Alert = useAlert()
@@ -22,6 +22,11 @@ function CreatePayment() {
     const data = await Payment.create(card.value, amount.value, refId.value, '', course.value)
 
     if(data) {
+      amount.clear()
+      card.clear()
+      refId.clear()
+      course.clear()
+
       Alert.pushMess('Success')
     } 
   }
@@ -31,7 +36,7 @@ function CreatePayment() {
         <div className={styles.form}>
           <h3 className={styles.title}>Make Payment</h3>
           <Input input={card} type='text' placeholder='card' />
-          <Input input={amount} type='text' placeholder='0' />
+          <Input input={amount} type='text' placeholder='Amount' />
           <Input input={course} type='text' placeholder='Course' />
           <Input input={refId} type='text' placeholder='Ref Id' />
           <button onClick={createHandler}>Create</button>
