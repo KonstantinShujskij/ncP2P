736970f01a5c76b29d026fc9388106db543d50d1
Konstantin <shujskij.konstantin@gmail.com>
2025-04-03 14:44:41 +0200

    fix

--- utils/Kvits.utils.js
+++ utils/Kvits.utils.js
@@ -18,9 +18,11 @@ const check = async (type, url, number) => {
         const data = await response.json()
         console.log(data)        
 
-        logs.res = JSON.parse(data)   
-        logs.time = Date.now() - logs.time
-        logs.statusCode = (data && data.card && data.amount)? 200 : 'Have not data'
+        if(data) {
+            logs.res = JSON.parse(data)
+            logs.time = Date.now() - logs.time
+            logs.statusCode = (data && data.card && data.amount)? 200 : 'Have not data'
+        }
 
         await Log.create(logs)
 
